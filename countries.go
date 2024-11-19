472a1cc7cc23e62af0aa823c9158ffdbb1778c15
Hamed Haghi <hamedhaghi07@gmail.com>
2024-11-19 21:28:18 +0100

    update countries and test file

--- countries.go
+++ countries.go
@@ -4,19 +4,18 @@ import (
 	"encoding/json"
 )
 
-type Name struct {
-	Common   string `json:"common"`
-	Official string `json:"official"`
-}
-
-type Currency struct {
-	Name   string `json:"name"`
-	Symbol string `json:"symbol"`
-}
-
 type Country struct {
-	Name       Name                `json:"name"`
-	Currencies map[string]Currency `json:"currencies"`
+	Capital   []string          `json:"capital"`
+	Languages map[string]string `json:"languages"`
+	Flags     map[string]string `json:"flags"`
+	Name      struct {
+		Common   string `json:"common"`
+		Official string `json:"official"`
+	} `json:"name"`
+	Currencies map[string]struct {
+		Name   string `json:"name"`
+		Symbol string `json:"symbol"`
+	} `json:"currencies"`
 }
 
 func All() (countries []Country, err error) {
@@ -29,7 +28,7 @@ func All() (countries []Country, err error) {
 }
 
 func ByName(name string) (country Country, err error) {
-	s, err := get("/name/" + name)
+	s, err := get("/name/" + name + "?fullText=true")
 	if err != nil {
 		return
 	}
@@ -43,6 +42,28 @@ func ByName(name string) (country Country, err error) {
 
 func ByRegion(region string) (countries []Country, err error) {
 	s, err := get("/region/" + region)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal([]byte(s), &countries)
+	return
+}
+
+func ByCapital(capital string) (country Country, err error) {
+	s, err := get("/capital/" + capital)
+	if err != nil {
+		return
+	}
+	countries := []Country{}
+	err = json.Unmarshal([]byte(s), &countries)
+	if len(countries) > 0 {
+		country = countries[0]
+	}
+	return
+}
+
+func ByLanguage(language string) (countries []Country, err error) {
+	s, err := get("/lang/" + language)
 	if err != nil {
 		return
 	}
--- countries_test.go
+++ countries_test.go
@@ -9,19 +9,56 @@ import (
 )
 
 func TestAll(t *testing.T) {
+	t.Parallel()
 	countries, err := countries.All()
 	assert.NoError(t, err)
 	assert.NotEmpty(t, countries)
 }
 
 func TestName(t *testing.T) {
+	t.Parallel()
 	country, err := countries.ByName("germany")
 	assert.NoError(t, err)
-	assert.Equal(t, "germany", strings.ToLower(country.Name.Common))
+	assert.Equal(t, "germany", strings.ToLower(string(country.Name.Common)))
 }
 
 func TestRegion(t *testing.T) {
+	t.Parallel()
 	countries, err := countries.ByRegion("europe")
 	assert.NoError(t, err)
 	assert.NotEmpty(t, countries)
+}
+
+func TestCapital(t *testing.T) {
+	t.Parallel()
+	country, err := countries.ByCapital("berlin")
+	assert.NoError(t, err)
+	assert.Equal(t, "berlin", strings.ToLower(country.Capital[0]))
+}
+
+func TestLanguage(t *testing.T) {
+	t.Parallel()
+	countries, err := countries.ByLanguage("german")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, countries)
+	assert.Equal(t, "german", strings.ToLower(countries[0].Languages["deu"]))
+}
+
+func TestFlagExists(t *testing.T) {
+	t.Parallel()
+	country, err := countries.ByName("germany")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, country)
+	assert.NotEmpty(t, country.Flags["png"])
+	assert.NotEmpty(t, country.Flags["svg"])
+
+}
+
+func TestCurrencyExists(t *testing.T) {
+	t.Parallel()
+	country, err := countries.ByName("germany")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, country)
+	assert.NotEmpty(t, country.Currencies["EUR"])
+	assert.NotEmpty(t, country.Currencies["EUR"].Symbol)
 }
